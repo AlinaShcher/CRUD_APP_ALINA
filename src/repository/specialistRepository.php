ae6670adae73c9ef9533c27d72f289c0f972391c
AlinaShcher <blinary98@mail.ru>
2024-10-24 17:21:08 +0500

    Update specialistRepository.php

--- src/repository/specialistRepository.php
+++ src/repository/specialistRepository.php
@@ -1,1 +1,36 @@
 <?php
+use Doctrine\ORM\EntityRepository;
+
+class specialistRepository extends EntityRepository{
+
+    public function save(Specialist $specialist){
+        $em = $this->getEntityManager();
+        $em->persist($specialist);
+        $em->flush();
+    }
+    public function delete(Specialist $specialist){
+        $em = $this->getEntityManager();
+        $em->remove($specialist);
+        $em->flush();
+    }
+    public function update(Specialist $specialist){
+        $em = $this->getEntityManager();
+        $em->persist($specialist);
+        $em->flush();
+    }
+    #ещё нашла вот такой вариант реализации find по id
+    public function findById(int $id): ?Specialist{
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select('s')
+            ->from('specialist', 's')
+            ->where('s.id = :id')
+            ->setParameter('id', $id);
+        return $qb->getQuery()->getOneOrNullResult();
+
+    }
+    public function findAll(): array
+    {
+        return $this->findBy([], ['name' => 'ASC']);
+    }
+}
