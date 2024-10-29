d4c45bb00d0c3056509744046ca08a0b1942b73c
AlinaShcher <blinary98@mail.ru>
2024-10-30 03:45:21 +0500

    Update SpecialistController.php

--- src/controller/SpecialistController.php
+++ src/controller/SpecialistController.php
@@ -1,98 +1,42 @@
 <?php
+namespace machine\src\controller\clientController;
+use src\entity\Client;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-require_once "bootstrap.php";
-use SrcEntitySpecialist;
-use SrcRepositoryProductSpecialist;
+use SrcEntityClient;
+use SrcFormClientType;
+use SrcRepositoryClientRepository;
 use SymfonyBundleFrameworkBundleControllerAbstractController;
 use SymfonyComponentHttpFoundationRequest;
 use SymfonyComponentHttpFoundationResponse;
 use SymfonyComponentRoutingAnnotationRoute;
 
-class SpecialistController extends AbstractController
+
+class clientController extends AbstractController
 {
-    private $specialistRepository;
-
-    public function __construct(SpecialistRepository $specialistRepository)
+    public function __construct()
     {
-        $this->specialistRepository = $specialistRepository;
+
     }
 
-    public function index(): Response
+    public function create(EntityManagerInterface $entityManager)
     {
-        $specialists = $this->specialistRepository->findAll();
-# как я понимаю render это метод наследуемый из absractcomtroller,
-        return $this->render('specialist/index.html.twig', [
-            'specialists' => $specialists,
-        ]);
+            $client = new Client();
+
+            $entityManager->persist($client);
+            $entityManager->flush();
+
+            return new Response('Saved new client with id ' . $client->getId());
+
     }
 
-    public function new(Request $request): Response
+
+    public function show(EntityManagerInterface $entityManager, int $id): Response
     {
-        $specialist = new Specialist();
-        $form = $this->createForm(SpecialistType::class, $specialist);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->specialistRepository->save($specialist);
-
-            return $this->redirectToRoute('specialist_index');
-        }
-#передаю данные в view шаблон
-        return $this->render('specialist/new.html.twig', [
-            'specialist' => $specialist,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    public function show(int $id): Response
-    {
-        $specialist = $this->specialistRepository->findById($id);
-
-        if (!$specialist) {
-            throw $this->createNotFoundException('Specialist not found.');
-        }
-
-        return $this->render('specialist/show.html.twig', [
-            'specialist' => $specialist,
-        ]);
-    }
-
-    public function edit(Request $request, int $id): Response
-    {
-        $specialist = $this->specialistRepository->findById($id);
-
-        if (!$specialist) {
-            throw $this->createNotFoundException('Specialist not found.');
-        }
-
-        $form = $this->createForm(SpecialistType::class, $specialist);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->specialistRepository->update($specialist);
-
-            return $this->redirectToRoute('specialist_index');
-        }
-
-        return $this->render('specialist/edit.html.twig', [
-            'specialist' => $specialist,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    public function delete(Request $request, int $id): Response
-    {
-        $specialist = $this->specialistRepository->findById($id);
-
-        if (!$specialist) {
-            throw $this->createNotFoundException('Specialist not found.');
-        }
-
-        if ($this->isCsrfTokenValid('delete' . $specialist->getId(), $request->request->get('_token'))) {
-            $this->specialistRepository->delete($specialist);
-        }
-
-        return $this->redirectToRoute('specialist_index');
+        $client = $entityManager->getRepository(Client::class)->find($id);
+        return new Response($client->getFullname());
     }
 }
-
