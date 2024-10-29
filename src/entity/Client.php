8342c28db84629ea3483d5c872bf16bf1b103cca
AlinaShcher <blinary98@mail.ru>
2024-10-30 03:47:51 +0500

    Update Client.php

--- src/entity/Client.php
+++ src/entity/Client.php
@@ -3,24 +3,26 @@ namespace src\entity\Client;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Type;
+use src\entity\Product\Product;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
 #[ORM\Table(name: 'client')]
 class Client
 {
     #[ORM\Id] # первичный ключ сущности
-    #[ORM\Column(type: 'integer', name:'id')]
-    #[ORM\GeneratedValue] # автоматически генерирует значения для первичного ключа
-    #[ORM\ManyToOne(targetEntity: Product::class, inversedBy: 'idClient')]
+    #[ORM\Column(Types::INTEGER)]// name:'id'
+    #[ORM\GeneratedValue(strategy:"SEQUENCE")] # автоматически генерирует значения для первичного ключа
+    #[ManyToMany(targetEntity: Product::class, mappedBy: 'idClient')]
     private ?int $id=null;
 
-    #[ORM\Column(type: 'string', name:'fullName')]
+    #[ORM\Column(Types:: STRING)]// , name:'fullName'
     private string $fullName;
 
-    #[ORM\Column(type: 'string', name: 'sex')]
+    #[ORM\Column(Types:: STRING)]//, name: 'sex'
     private string $sex;
 
-    #[ORM\Column(type: 'int', name: 'phoneName')]
+    #[ORM\Column(Types::INTEGER)]// , name: 'phoneName'
     private string $phoneNumber;
 # но я не понимаю как здесь тогла сделать construct? через продукты? Или оставить всё как было?
     private Collection $products;
