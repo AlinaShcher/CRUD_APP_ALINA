4758cc978ac3d6941c7f506cd64ad48232e6224f
AlinaShcher <blinary98@mail.ru>
2024-11-01 01:14:41 +0500

    Update ProductService.php

--- src/service/ProductService.php
+++ src/service/ProductService.php
@@ -1,1 +1,19 @@
+<?php
+namespace app\service\ProductService;
+use src\repository\ProductRepository;
+use src\entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+class ProductService
+{
 
+    public function create(EntityManagerInterface $entityManager)
+    {
+        $product = new Product();
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return new Response('Saved new product with id ' . $product->getId());
+
+    }
+}
