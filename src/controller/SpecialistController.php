5e3fc61cc6049a747b5cc66aa219834538bb617e
AlinaShcher <blinary98@mail.ru>
2024-11-03 20:41:05 +0500

    Update SpecialistController.php

--- src/controller/SpecialistController.php
+++ src/controller/SpecialistController.php
@@ -1,103 +1,25 @@
 <?php
-namespace machine\src\controller\SpecialistController; ;
-
+namespace src\controller\SpecialistController; ;
 use src\entity\Specialist;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
-
-
-
-
-class SpecialistController extends AbstractController
+use src\service\SpecialistService;
+class SpecialistController
 {
-    private $specialistRepository;
+    private SpecialistService $specialistservice;
 
-    public function __construct(SpecialistRepository $specialistRepository)
+    public function __construct(SpecialistService $specialistservice)
     {
-        $this->specialistRepository = new $SpecialistRepository ();
+        $this->SpecialistService = $specialistservice;
+    }
+    
+    public function deleteSpecialist()
+    {
+        if (isset($_REQUEST['id']))
+        {
+            $this->specialistservice->deleteSpecialist($_REQUEST['id']);
+        } else {
+            echo 'Ошибка';
+        }
     }
 
-    public function index(): Response
-    {
-        $specialists = $this->specialistRepository->findAll();
-# как я понимаю render это метод наследуемый из absractcomtroller,
-        return $this->render('specialist/index.html.twig', [
-            'specialists' => $specialists,
-        ]);
-    }
 
-    public function new(Request $request): Response
-    {
-        $specialist = new Specialist();
-        $form = $this->createForm(SpecialistType::class, $specialist);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->specialistRepository->save($specialist);
-
-            return $this->redirectToRoute('specialist_index');
-        }
-#передаю данные в view шаблон
-        return $this->render('specialist/new.html.twig', [
-            'specialist' => $specialist,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    public function show(int $id): Response
-    {
-        $specialist = $this->specialistRepository->findById($id);
-
-        if (!$specialist) {
-            throw $this->createNotFoundException('Specialist not found.');
-        }
-
-        return $this->render('specialist/show.html.twig', [
-            'specialist' => $specialist,
-        ]);
-    }
-
-    public function edit(Request $request, int $id): Response
-    {
-        $specialist = $this->specialistRepository->findById($id);
-
-        if (!$specialist) {
-            throw $this->createNotFoundException('Specialist not found.');
-        }
-
-        $form = $this->createForm(SpecialistType::class, $specialist);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->specialistRepository->update($specialist);
-
-            return $this->redirectToRoute('specialist_index');
-        }
-
-        return $this->render('specialist/edit.html.twig', [
-            'specialist' => $specialist,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    public function delete(Request $request, int $id): Response
-    {
-        $specialist = $this->specialistRepository->findById($id);
-
-        if (!$specialist) {
-            throw $this->createNotFoundException('Specialist not found.');
-        }
-
-        if ($this->isCsrfTokenValid('delete' . $specialist->getId(), $request->request->get('_token'))) {
-            $this->specialistRepository->delete($specialist);
-        }
-
-        return $this->redirectToRoute('specialist_index');
-    }
 }
