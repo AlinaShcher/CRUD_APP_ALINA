609549f766f5c104def194a6719f07cc5fc8081a
AlinaShcher <blinary98@mail.ru>
2024-11-06 04:25:25 +0500

    Update SpecialistService.php

--- src/service/SpecialistService.php
+++ src/service/SpecialistService.php
@@ -1,54 +1,26 @@
 <?php
-namespace src\service\ProductService;
-use src\repository\ProductRepository;
-use src\entity\Product;
-use src\entity\Client;
+namespace src\service\SpecialistService;
+use src\repository\SpecialistRepository;
 use src\entity\Specialist;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-class ProductService
+class SpecialistService
 {
+    private $entityManager;
+
     public function __construct()
     {
         $this->entityManager = getEntityManager();
     }
-    public function createProduct(int $id, int $idClient, int $idSpecialist)
+
+    public function deleteSpecialist (int $id)
     {
         try {
-            if ($id != 0)
-            {
-                $product = $this->entityManager->getRepository(Product::class)->find($id);
-            } else {
-                $product = new Product();
-            }
+            $specialist = $this->entityManager->find($id);
+            $this->entityManager->remove($specialist);
 
-            $idc = $this->entityManager->gerRepository(Client::class)->find($idClient);
-            $ids = $this->entityManager->gerRepository(Specialist::class)->find($idSpecialist);
-            $product-> setIdclient($idc);
-            $product-> setIdspecialist($ids);
-            $this->entityManager->persist($product);
-            $this->entityManager->flush();
-        } catch(Exception $e) {
-
-        }
-    }
-    public function deleteProduct( int $id)
-    {
-        try {
-            $product = $this->entityManager->getRepository(Product::class)->find($id);
-            $this->entityManager->remove($product);
-            $this->entityManager->flush();
-        } catch(Exception $e) {
-
-        }
-    }
-
-    public function showProduct(int $id)
-    {
-        try {
-            $product = $this->entityManager->getRepository(Product::class)->find($id);
-            return $product;
         } catch (Exception $e) {
-
+            echo "Ошибка: " . $e->getMessage();
         }
     }
 }
