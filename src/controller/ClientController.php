e9bdba17e810f3b9e86c26898c220d83d28cbd45
AlinaShcher <blinary98@mail.ru>
2024-11-18 00:36:11 +0500

    Update ClientController.php

--- src/controller/ClientController.php
+++ src/controller/ClientController.php
@@ -1,16 +1,16 @@
 <?php
 namespace Src\Controller\ClientController;
-use Src\Entity\Client;
 use Src\Service\ClientService;
 use Exception;
-
 class ClientController
 {
     private ClientService $clientService;
+    
     public function __construct(ClientService $clientService)
     {
         $this->ClientService = $clientService;
     }
+    
     public function deleteClient(){
         if (!isset($_REQUEST['id'])) {
             throw new Exception("Неверные параметры запроса", 400);
