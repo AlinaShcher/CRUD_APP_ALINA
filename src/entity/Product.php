8bb76901bd5c0f8e3ea4bbc68eac6103c957f610
AlinaShcher <blinary98@mail.ru>
2024-11-18 00:46:03 +0500

    Update Product.php

--- src/entity/Product.php
+++ src/entity/Product.php
@@ -2,8 +2,8 @@
 namespace Src\Entity\Product;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Src\Entity\Client\Client;
-use Src\Entity\Specialist\Specialist;
+use Src\Entity\Client;
+use Src\Entity\Specialist;
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\ManyToOne;
 
@@ -16,66 +16,79 @@ class Product
     #[ORM\GeneratedValue(strategy:"SEQUENCE")]
     #[ORM\SequenceGenerator]
     private int $id;
+    
     #[ManyToOne(targetEntity: Client::class, fetch: 'EAGER')]
-    #[JoinColumn(name: 'Client', referencedColumnName: 'id')]
-    private Client $Client;
+    #[JoinColumn(name: 'client', referencedColumnName: 'id')]
+    private Client $client;
+    
     #[ManyToOne(targetEntity: Specialist::class, fetch: 'EAGER')]
-    #[JoinColumn(name: 'Specialist', referencedColumnName: 'id')]
-    private Specialist $Specialist;
+    #[JoinColumn(name: 'specialist', referencedColumnName: 'id')]
+    private Specialist $specialist;
 
-    #[ORM\Column(name: 'model', type: Types:: STRING)]//name: ''
+    #[ORM\Column(name: 'model', type: Types:: STRING)]
     private string $model;
 
-    #[ORM\Column(name: 'status', type: Types:: STRING)]//name: ''
+    #[ORM\Column(name: 'status', type: Types:: STRING)]
     private string $status;
 
-    #[ORM\Column(name: 'cost', type: Types::STRING)]//name: ''
+    #[ORM\Column(name: 'cost', type: Types::STRING)]
     private string $cost;
 
     public function getId(): ?int
     {
         return $this->id;
     }
+    
     public function getClient(): ?int
     {
-        return $this->Client;
+        return $this->client;
     }
+    
     public function getSpecialist(): ?int
     {
-        return $this->Specialist;
+        return $this->specialist;
     }
+    
     public function getModel(): string {
         return $this->model;
     }
+    
     public function getStatus(): string
     {
         return $this->status;
     }
+    
     public function getCost(): string
     {
         return $this->cost;
     }
+    
     public function setId(int $id): product
     {
         $this->id = $id;
         return $this;
     }
-    public function setClient(int $Client): void
+    
+    public function setClient(int $client): void
     {
-        $this->idClient = $Client;
+        $this->client = $client;
     }
-    public function setSpecialist(int $Specialist): void
+    
+    public function setSpecialist(int $specialist): void
     {
-        $this->idSpecialist = $Specialist;
+        $this->specialist = $specialist;
     }
+    
     public function setModel(string $model): void
     {
         $this->model = $model;
     }
+    
     public function setStatus(string $status): void
     {
         $this->status = $status;
     }
+    
     public function setCost(string $cost): void {
         $this->cost = $cost;
     }
