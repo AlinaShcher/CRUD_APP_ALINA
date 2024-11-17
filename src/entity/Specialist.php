3d114384e0f814900f911662ba4d032c9b7e8444
AlinaShcher <blinary98@mail.ru>
2024-11-18 00:02:28 +0500

    Update Specialist.php

--- src/entity/Specialist.php
+++ src/entity/Specialist.php
@@ -1,5 +1,5 @@
 <?php
-namespace src\entity\Specialist;
+namespace Src\Entity\Specialist;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\DBAL\Types\Types;
