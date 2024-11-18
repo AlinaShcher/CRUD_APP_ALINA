dc4340750017e6c2bf7ccb58b349796e2ea21946
AlinaShcher <blinary98@mail.ru>
2024-11-19 03:57:34 +0500

    Update ProductRepository.php

--- src/repository/ProductRepository.php
+++ src/repository/ProductRepository.php
@@ -7,7 +7,7 @@ class ProductRepository extends EntityRepository {
 
     public function getBestSpecialist()
     {
-        $qb = $this->EntityManager->createQueryBuilder();
+        $qb = $this->createQueryBuilder('p');
         $qb ->select('p.idSpecialist', 'COUNT(*) as count')
             ->from('Product','p')
             -> groupBy('p.idSpecialist')
