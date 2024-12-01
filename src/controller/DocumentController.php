8e38f8503cbab9a5ad9bb10c06a9b2826f6e2aed
AlinaShcher <blinary98@mail.ru>
2024-12-01 03:24:12 +0300

    Add files via upload

---
+++ src/controller/DocumentController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Src\Controller\DocumentController;
+
+use Doctrine\ORM\Exception\ORMException;
+use Src\Service\DocumentService;
+
+
+class DocumentController
+{
+    private DocumentService $documentService;
+
+    public function __construct()
+    {
+        $this->documentService = new DocumentService();
+    }
+    public function htmlGet()
+    {
+        try{
+            $this->documentService->htmlGet();
+        } catch(ORMException $e) {
+            throw new ("Ошибка: " . $e->getMessage());
+        }
+    }
+}
